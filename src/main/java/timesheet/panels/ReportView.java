b4cf584b9655cd33d38f77838a5eadcc980c33d4
Adam1901 <adamlmeadows@googlemail.com>
2016-06-17 20:41:47 +0100

    Actually added the ability to create reports

--- src/main/java/timesheet/panels/ReportView.java
+++ src/main/java/timesheet/panels/ReportView.java
@@ -1,7 +1,264 @@
 package timesheet.panels;
 
 import javax.swing.JPanel;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.GridBagConstraints;
+import javax.swing.UIManager;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.border.BevelBorder;
+import java.awt.Insets;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.MatteBorder;
+import javax.swing.border.TitledBorder;
+
+import org.jdatepicker.JDatePicker;
+import org.jdatepicker.impl.JDatePanelImpl;
+import org.jdatepicker.impl.JDatePickerImpl;
+import org.jdatepicker.impl.UtilDateModel;
+import org.joda.time.DateTime;
+
+import timesheet.Application;
+import timesheet.DTO.DTOProject;
+import timesheet.DTO.DTOResource;
+import timesheet.connection.DBEngine.DbEngine;
+import timesheet.connection.DBEngine.Report;
+
+import javax.swing.JComboBox;
+import javax.swing.JFormattedTextField;
+import javax.swing.JFormattedTextField.AbstractFormatter;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.awt.event.ActionEvent;
+import javax.swing.JCheckBox;
+import javax.swing.JTextPane;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 
 public class ReportView extends JPanel {
+	private static final long serialVersionUID = 1L;
 
+	JComboBox<DTOProject> cmbProjectList = new JComboBox<DTOProject>();
+	JComboBox<DTOResource> cmbUserList = new JComboBox<DTOResource>();
+
+	JTextPane textPane;
+
+	public ReportView() throws SQLException {
+		jbInit();
+		fillCmbBoxes();
+	}
+
+	private void fillCmbBoxes() throws SQLException {
+		DbEngine db = new DbEngine();
+		List<DTOProject> allProject = db.getAllProject();
+		List<DTOResource> allResources = db.getAllResources();
+
+		for (DTOResource dtoResource : allResources) {
+			cmbUserList.addItem(dtoResource);
+		}
+		for (DTOProject dtoProject : allProject) {
+
+			cmbProjectList.addItem(dtoProject);
+		}
+	}
+
+	private void jbInit() {
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[] { 5, 0, 0, 0, 0, 0, 5, 0 };
+		gridBagLayout.rowHeights = new int[] { 5, 0, 0, 0, 0, 0, 5, 0 };
+		gridBagLayout.columnWeights = new double[] { 1.0, 1.0, 0.0, 1.0, 0.0, 1.0, 1.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, Double.MIN_VALUE };
+		setLayout(gridBagLayout);
+
+		JLabel lblNewLabel = new JLabel("User");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.fill = GridBagConstraints.HORIZONTAL;
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 1;
+		gbc_lblNewLabel.gridy = 1;
+		add(lblNewLabel, gbc_lblNewLabel);
+
+		GridBagConstraints gbc_cmbUserList = new GridBagConstraints();
+		gbc_cmbUserList.gridwidth = 4;
+		gbc_cmbUserList.insets = new Insets(0, 0, 5, 5);
+		gbc_cmbUserList.fill = GridBagConstraints.HORIZONTAL;
+		gbc_cmbUserList.gridx = 2;
+		gbc_cmbUserList.gridy = 1;
+		add(cmbUserList, gbc_cmbUserList);
+
+		Properties p = new Properties();
+		p.put("text.today", "Today");
+		p.put("text.month", "Month");
+		p.put("text.year", "Year");
+		JDatePanelImpl datePanel = new JDatePanelImpl(new UtilDateModel(), p);
+
+		Properties p1 = new Properties();
+		p1.put("text.today1", "Today");
+		p1.put("text.month1", "Month");
+		p1.put("text.year1", "Year");
+		JDatePanelImpl datePanel2 = new JDatePanelImpl(new UtilDateModel(), p);
+
+		JLabel lblNewLabel_1 = new JLabel("StartDate:");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 1;
+		gbc_lblNewLabel_1.gridy = 2;
+		add(lblNewLabel_1, gbc_lblNewLabel_1);
+		// Don't know about the formatter, but there it is...
+		JDatePickerImpl startDatePicker = new JDatePickerImpl(datePanel, new DateLabelFormatter());
+
+		GridBagConstraints gbc_Date1 = new GridBagConstraints();
+		gbc_Date1.gridwidth = 2;
+		gbc_Date1.insets = new Insets(0, 0, 5, 5);
+		gbc_Date1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_Date1.gridx = 2;
+		gbc_Date1.gridy = 2;
+		add(startDatePicker, gbc_Date1);
+
+		JLabel lblEndDate = new JLabel("End Date:");
+		GridBagConstraints gbc_lblEndDate = new GridBagConstraints();
+		gbc_lblEndDate.anchor = GridBagConstraints.EAST;
+		gbc_lblEndDate.insets = new Insets(0, 0, 5, 5);
+		gbc_lblEndDate.gridx = 4;
+		gbc_lblEndDate.gridy = 2;
+		add(lblEndDate, gbc_lblEndDate);
+
+		JDatePickerImpl endDatePicker = new JDatePickerImpl(datePanel2, new DateLabelFormatter());
+		GridBagConstraints gbc_Date2 = new GridBagConstraints();
+		gbc_Date2.insets = new Insets(0, 0, 5, 5);
+		gbc_Date2.fill = GridBagConstraints.HORIZONTAL;
+		gbc_Date2.gridx = 5;
+		gbc_Date2.gridy = 2;
+		add(endDatePicker, gbc_Date2);
+
+		JLabel lblProject = new JLabel("Project");
+		GridBagConstraints gbc_lblProject = new GridBagConstraints();
+		gbc_lblProject.fill = GridBagConstraints.HORIZONTAL;
+		gbc_lblProject.insets = new Insets(0, 0, 5, 5);
+		gbc_lblProject.gridx = 1;
+		gbc_lblProject.gridy = 3;
+		add(lblProject, gbc_lblProject);
+
+		JCheckBox chckbxNewCheckBox = new JCheckBox("Use All projects?");
+		chckbxNewCheckBox.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (chckbxNewCheckBox.isSelected()) {
+					cmbProjectList.setEnabled(false);
+					cmbProjectList.setSelectedIndex(-1);
+				} else {
+					cmbProjectList.setEnabled(true);
+					cmbProjectList.setSelectedIndex(0);
+				}
+			}
+		});
+		GridBagConstraints gbc_chckbxNewCheckBox = new GridBagConstraints();
+		gbc_chckbxNewCheckBox.insets = new Insets(0, 0, 5, 5);
+		gbc_chckbxNewCheckBox.gridx = 2;
+		gbc_chckbxNewCheckBox.gridy = 3;
+		add(chckbxNewCheckBox, gbc_chckbxNewCheckBox);
+
+		GridBagConstraints gbc_cmbProjectList = new GridBagConstraints();
+		gbc_cmbProjectList.gridwidth = 3;
+		gbc_cmbProjectList.insets = new Insets(0, 0, 5, 5);
+		gbc_cmbProjectList.fill = GridBagConstraints.HORIZONTAL;
+		gbc_cmbProjectList.gridx = 3;
+		gbc_cmbProjectList.gridy = 3;
+		add(cmbProjectList, gbc_cmbProjectList);
+
+		JButton btnRunReport = new JButton("Run Report");
+		btnRunReport.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				DbEngine db = new DbEngine();
+				try {
+					Report report = new Report();
+					if (chckbxNewCheckBox.isSelected()) {
+						report.setUseAllProjects(true);
+					} else {
+						report.setUseAllProjects(false);
+						report.setProject((DTOProject) cmbProjectList.getSelectedItem());
+					}
+					DTOResource selectedUser = (DTOResource) cmbUserList.getSelectedItem();
+					report.setResource(selectedUser);
+					report.setEnd(getDateTime(endDatePicker));
+					report.setStart(getDateTime(startDatePicker));
+					String runReport = db.runReport(report);
+					System.out.println(runReport);
+					StringBuilder stringBuilder = new StringBuilder();
+					stringBuilder.append(textPane.getText());
+					stringBuilder.append("\n");
+					stringBuilder.append(new SimpleDateFormat("dd/MM/yyyy").format(new Date()));
+					stringBuilder.append(": Report ran for: \"").append(selectedUser.getResourceName());
+					stringBuilder.append("\". For project: \"").append(chckbxNewCheckBox.isSelected() ? "All"
+							: ((DTOProject) cmbProjectList.getSelectedItem()).getProjectName());
+					stringBuilder.append("\". The amount of time looged is: ").append(runReport);
+					textPane.setText(stringBuilder.toString());
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		GridBagConstraints gbc_btnRunReport = new GridBagConstraints();
+		gbc_btnRunReport.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnRunReport.gridwidth = 5;
+		gbc_btnRunReport.insets = new Insets(0, 0, 5, 5);
+		gbc_btnRunReport.gridx = 1;
+		gbc_btnRunReport.gridy = 4;
+		add(btnRunReport, gbc_btnRunReport);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 5);
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridwidth = 5;
+		gbc_scrollPane.gridx = 1;
+		gbc_scrollPane.gridy = 5;
+		add(scrollPane, gbc_scrollPane);
+
+		textPane = new JTextPane();
+		scrollPane.setViewportView(textPane);
+		textPane.setEditable(false);
+	}
+
+	private DateTime getDateTime(JDatePickerImpl date) {
+		Date value = (Date) date.getModel().getValue();
+		return new DateTime(value.getTime());
+	}
+
+	public class DateLabelFormatter extends AbstractFormatter {
+		private static final long serialVersionUID = 1L;
+		private String datePattern = "dd/MM/yyyy";
+		private SimpleDateFormat dateFormatter = new SimpleDateFormat(datePattern);
+
+		@Override
+		public Object stringToValue(String text) throws ParseException {
+			return dateFormatter.parseObject(text);
+		}
+
+		@Override
+		public String valueToString(Object value) throws ParseException {
+			if (value != null) {
+				Calendar cal = (Calendar) value;
+				return dateFormatter.format(cal.getTime());
+			}
+
+			return "";
+		}
+
+	}
 }
